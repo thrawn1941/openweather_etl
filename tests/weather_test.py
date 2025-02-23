dad4233a5b1ddaa3c18446f521fd8cc9f4de2575
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-02-23 22:04:20 +0100

    feat: adding test file related to WeatherCurrentDataStrategy class

---
+++ tests/weather_test.py
@@ -0,0 +1,22 @@
+import pytest
+import requests
+from get_data.weather_get_data_strategy import WeatherCurrentDataStrategy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('OPEN_WEATHER_API_KEY')
+
+@pytest.fixture
+def get_test_data():
+    lat, lon = 52.2297, 21.0122
+
+    weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
+    weather_data = requests.get(weather_url).json()
+
+    return weather_data
+
+
+def test_weather_get_data(get_test_data):
+    result = WeatherCurrentDataStrategy().get_data('Warsaw', api_key)
+    assert result == get_test_data
