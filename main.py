12f45f6ca0fe0150ea17c89191033b4b11e869c0
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-03-04 00:36:58 +0100

    adding config file and get_temperature_data function

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from get_data.weather_get_data_strategy import WeatherCurrentDataStrategy
 from endpoint_class import Endpoint
 import functions_framework
 import base64
+import json
 
 
 GLOBAL_START_DATE=dt.datetime(2020, 12, 1, 0, 0)
@@ -41,11 +42,14 @@ def main():
     t1.join()
     t2.join()
 
-    a = app_pollution.collected_data.get('Warsaw')
-    b = app_pollution.collected_data.get('London')
+    a = app_weather.collected_data.get('Warsaw')
+    b = app_weather.collected_data.get('London')
+    c = app_weather.get_temperature()
     print(a)
     print("---------------------------------------------")
     print(b)
+    print("---------------------------------------------")
+    print(c)
 
 @functions_framework.http
 def get_warsaw_temperature(_):
@@ -61,5 +65,17 @@ def get_warsaw_temperature(_):
 def hello_pubsub(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
     print(base64.b64decode(cloud_event.data["message"]["data"]))
+
+@functions_framework.http
+def get_temperature_data(_):
+    cities = []
+    with open("config", "r", encoding="utf-8") as f:
+        for line in f:
+            cities.append(line.strip())
+
+    app_weather = Endpoint(WeatherCurrentDataStrategy())
+    gathered_data = app_weather.append_data_from_cities(API_KEY, *cities)
+    result = json.dumps(gathered_data)
+    return result, 200
 
 main()
