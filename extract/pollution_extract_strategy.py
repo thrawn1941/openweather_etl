1ca0f9faff3d19fadf867b4df7718c7761dc9842
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-03-23 17:23:32 +0100

    refactor(enpoint_class): replacing static arguments in get_data() methods with *args; removing redunant methods from endpoint class

--- extract/pollution_extract_strategy.py
+++ extract/pollution_extract_strategy.py
@@ -9,7 +9,8 @@ class AirPollutionDataStrategy(GetDataStrategy):
     def __name__(self):
         return type(self).__name__
 
-    def get_data(self, city, api_key):
+    def get_data(self, *args):
+        city, api_key = args[0], args[1]
         lat, lon = GeoDirectDataStrategy.get_lat_and_lon(city, api_key)
         pollution_url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}'
         pollution_data = requests.get(pollution_url).json()
