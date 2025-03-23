ca9e4e8c74e5306b6d1a36bc54d68025750f7064
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-03-23 21:02:09 +0100

    fix(terraform): renaming google_storage_bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,54 +11,54 @@ provider "google" {
   region=var.region
   credentials = var.gcp_credentials
 }
-resource "google_workflows_workflow" "default" {
-  name            = "test-workflow"
-  region          = "europe-central2"
-  description     = "A test workflow"
-  service_account = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+# resource "google_workflows_workflow" "default" {
+#   name            = "test-workflow"
+#   region          = "europe-central2"
+#   description     = "A test workflow"
+#   service_account = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+#
+#   source_contents = <<-EOF
+#     - init:
+#         assign:
+#             - results : {} # result from each iteration keyed by table name
+#             - functions:
+#                 - get_geo_data
+#                 - get_last_day_pollution_data
+#                 #- get_last_month_pollution_data
+#                 - get_last_week_pollution_data
+#                 - get_pollution_data
+#                 - get_weather_data
+#     - runQueries:
+#         parallel:
+#             shared: [results]
+#             for:
+#                 value: func
+#                 in: $${functions}
+#                 steps:
+#                   - call_function:
+#                       call: http.get
+#                       args:
+#                         url: $${"https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/" + func}
+#                         auth:
+#                           type: OIDC
+#                       result: function_response
+#                   - publish_to_pubsub:
+#                       call: googleapis.pubsub.v1.projects.topics.publish
+#                       args:
+#                         topic: projects/totemic-client-447220-r1/topics/demo-topic
+#                         body:
+#                           messages:
+#                             - data: $${base64.encode(text.encode(function_response.body))}
+#                       result: pubsub_response
+#                   - returnResult:
+#                       assign:
+#                           - results[func]: $${pubsub_response}
+#     - returnResults:
+#         return: $${results}
+# EOF
 
-  source_contents = <<-EOF
-    - init:
-        assign:
-            - results : {} # result from each iteration keyed by table name
-            - functions:
-                - get_geo_data
-                - get_last_day_pollution_data
-                #- get_last_month_pollution_data
-                - get_last_week_pollution_data
-                - get_pollution_data
-                - get_weather_data
-    - runQueries:
-        parallel:
-            shared: [results]
-            for:
-                value: func
-                in: $${functions}
-                steps:
-                  - call_function:
-                      call: http.get
-                      args:
-                        url: $${"https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/" + func}
-                        auth:
-                          type: OIDC
-                      result: function_response
-                  - publish_to_pubsub:
-                      call: googleapis.pubsub.v1.projects.topics.publish
-                      args:
-                        topic: projects/totemic-client-447220-r1/topics/demo-topic
-                        body:
-                          messages:
-                            - data: $${base64.encode(text.encode(function_response.body))}
-                      result: pubsub_response
-                  - returnResult:
-                      assign:
-                          - results[func]: $${pubsub_response}
-    - returnResults:
-        return: $${results}
-EOF
-
-}
-resource "google_storage_bucket" "bucket" {
+#}
+resource "google_storage_bucket" "bucket_for_functions" {
   name     = "functions-bucket"
   location = var.region
 }
