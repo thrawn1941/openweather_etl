9acad33c72c50aef12198ed46261d5833eda4e8e
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-03-31 22:53:01 +0200

    fix(terraform): one more fix to url

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,7 +115,7 @@ resource "google_cloud_scheduler_job" "test_workflow" {
   schedule    = "0 * * * *"
   time_zone   = "Europe/Warsaw"
   http_target {
-    uri         = "https://${google_workflows_workflow.test_workflow.id}"
+    uri         = "https://workflowexecutions.googleapis.com/v1/${google_workflows_workflow.test_workflow.id}/executions"
     http_method = "POST"
     oauth_token {
       service_account_email = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
