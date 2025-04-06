cb9b6378372adde9e9049d95b2fd0827b773e96f
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-04-07 00:00:11 +0200

    fix(terraform): creating test dataset; creating output file

--- terraform/bigQuery.tf
+++ terraform/bigQuery.tf
@@ -1,5 +1,17 @@
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "foo"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
 resource "google_bigquery_table" "default" {
-  dataset_id = "totemic-client-447220-r1.city_temperature_data_set"
+  dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "test_weather"
 
   time_partitioning {
