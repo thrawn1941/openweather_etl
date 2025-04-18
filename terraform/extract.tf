c9ec3891dd02413ca56f63c3eaa1e96a706bca3f
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-04-18 15:43:28 +0200

    feat(load): creating workflow for raw geo data

--- terraform/extract.tf
+++ terraform/extract.tf
@@ -123,6 +123,37 @@ resource "google_workflows_workflow" "test_workflow" {
   EOF
 
 }
+##### workflow for extract raw geo data
+resource "google_workflows_workflow" "geo_workflow" {
+  name            = "geo-workflow"
+  region          = "europe-central2"
+  description     = "Workflow exporting raw geo data to pubsub"
+  service_account = "test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+
+  source_contents = <<-EOF
+    main:
+      params: [input]
+      steps:
+        - call_function:
+            call: http.get
+            args:
+              url: https://europe-central2-totemic-client-447220-r1.cloudfunctions.net/export_raw_geo_to_bigquery_tf
+              auth:
+                type: OIDC
+            result: function_response
+        - publish_to_pubsub:
+            call: googleapis.pubsub.v1.projects.topics.publish
+            args:
+              topic: projects/totemic-client-447220-r1/topics/get_geo_data-topic
+              body:
+                messages:
+                  - data: $${base64.encode(text.encode(function_response.body))}
+            result: pubsub_response
+        - return_results:
+            return: $${pubsub_response}
+  EOF
+
+}
 #########################################################################################################
 ##### schedules for extract functions
 resource "google_cloud_scheduler_job" "test_workflow" {
