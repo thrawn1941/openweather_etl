3248b933096bc713c1fe6887f2bc4ce020c8cf09
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-04-27 22:02:47 +0200

    feat(load): adding load of historical pollution data in terraform

--- terraform/load.tf
+++ terraform/load.tf
@@ -37,10 +37,55 @@ resource "google_cloudfunctions2_function" "load_functions" {
     }
   }
 }
+resource "google_cloudfunctions2_function" "load_functions2" {
+  for_each = tomap({
+    "export_raw_pollution_to_bigquery2" = "get_pollution_data",
+  })
+
+  name        = "${each.key}_tf"
+  description = "Function for data load"
+  location    = var.region
+
+  build_config {
+    runtime = "python311"
+    entry_point = each.key
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket_for_functions.name
+        object = google_storage_bucket_object.archive.name
+      }
+    }
+  }
+
+  event_trigger {
+    trigger_region = var.region
+    event_type = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic = "projects/totemic-client-447220-r1/topics/get_historical_pollution_data"
+    retry_policy = "RETRY_POLICY_UNSPECIFIED"
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256Mi"
+    environment_variables = {
+      OPEN_WEATHER_API_KEY = var.open_weather_api_key
+    }
+  }
+}
 #########################################################################################################
 ##### invoker permissions for load functions
 resource "google_cloudfunctions2_function_iam_member" "invoker_load_functions" {
   for_each = google_cloudfunctions2_function.load_functions
+
+  project        = each.value.project
+  location       = each.value.location
+  cloud_function = each.value.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
+}
+resource "google_cloudfunctions2_function_iam_member" "invoker_load_functions" {
+  for_each = google_cloudfunctions2_function.load_functions2
 
   project        = each.value.project
   location       = each.value.location
