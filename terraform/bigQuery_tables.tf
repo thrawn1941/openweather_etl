702fb7b67ab8d27ffd597d7c0dbe2e2cd18912ce
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-07 15:23:41 +0200

    fix(terraform): adding var. to schema value

--- terraform/bigQuery_tables.tf
+++ terraform/bigQuery_tables.tf
@@ -3,7 +3,7 @@
 resource "google_bigquery_table" "weather_raw" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "weather_raw"
-  schema = jsondecode(bq_weather_schema)
+  schema = jsondecode(var.bq_weather_schema)
 }
 
 resource "google_bigquery_table" "pollution_raw" {
