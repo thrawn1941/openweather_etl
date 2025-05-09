6ba446798a14bec481320702e65da22e056a1479
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-10 00:16:27 +0200

    fix(terraform): unused references

--- terraform/function_iam_member.tf
+++ terraform/function_iam_member.tf
@@ -1,24 +1,4 @@
 #########################################################################################################
-##### invoker permissions for extract functions
-resource "google_cloudfunctions2_function_iam_member" "invoker" {
-  for_each = google_cloudfunctions2_function.extract_functions
-
-  project        = each.value.project
-  location       = each.value.location
-  cloud_function = each.value.name
-
-  role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
-}
-resource "google_cloudfunctions2_function_iam_member" "invoker_extract_last_month" {
-  project        = google_cloudfunctions2_function.extract_historical_pollution.project
-  location       = google_cloudfunctions2_function.extract_historical_pollution.location
-  cloud_function = google_cloudfunctions2_function.extract_historical_pollution.name
-
-  role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:test-account@totemic-client-447220-r1.iam.gserviceaccount.com"
-}
-#########################################################################################################
 ##### invoker permissions for load functions
 resource "google_cloudfunctions2_function_iam_member" "invoker_load_functions" {
   for_each = google_cloudfunctions2_function.load_functions
--- terraform/pubsub_topics.tf
+++ terraform/pubsub_topics.tf
@@ -1,12 +1,4 @@
-resource "google_pubsub_topic" "extract_functions_topics" {
-  for_each = toset(var.extract_functions)
-  name = "${each.key}-topic"
-}
 ### historical_pollution_data
 resource "google_pubsub_topic" "extract_historical_pollution" {
   name = "get_historical_pollution_data"
-}
-### historical_weather_data
-resource "google_pubsub_topic" "extract_historical_weather" {
-  name = "get_historical_weather_data"
 }
