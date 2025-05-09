ae94c4e8e5c952da7aa70dd4772da16dc754b9ed
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-10 00:59:45 +0200

    fix(terraform): memory increased for extract functions

--- terraform/extract_functions.tf
+++ terraform/extract_functions.tf
@@ -7,7 +7,7 @@ locals {
       storage_bucket_name          = google_storage_bucket.bucket_for_functions.name
       storage_bucket_object_name   = google_storage_bucket_object.archive.name
       instances                    = 1
-      memory                       = "128Mi"
+      memory                       = "256Mi"
       env_vars                     = {
         OPEN_WEATHER_API_KEY = var.open_weather_api_key
       }
@@ -21,7 +21,7 @@ locals {
       storage_bucket_name          = google_storage_bucket.bucket_for_functions.name
       storage_bucket_object_name   = google_storage_bucket_object.archive.name
       instances                    = 1
-      memory                       = "128Mi"
+      memory                       = "256Mi"
       env_vars                     = {
         OPEN_WEATHER_API_KEY = var.open_weather_api_key
       }
@@ -35,7 +35,7 @@ locals {
       storage_bucket_name          = google_storage_bucket.bucket_for_functions.name
       storage_bucket_object_name   = google_storage_bucket_object.archive.name
       instances                    = 1
-      memory                       = "128Mi"
+      memory                       = "256Mi"
       env_vars                     = {
         OPEN_WEATHER_API_KEY = var.open_weather_api_key
       }
