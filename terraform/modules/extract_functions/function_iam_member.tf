509737d86fe53a59c9d118b5193185d2ccae3c79
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-09 09:38:25 +0200

    fix(terraform): removing for_each in module`s iam_member

--- terraform/modules/extract_functions/function_iam_member.tf
+++ terraform/modules/extract_functions/function_iam_member.tf
@@ -1,9 +1,9 @@
 resource "google_cloudfunctions2_function_iam_member" "default_invoker" {
-  for_each = google_cloudfunctions2_function.default_extract
+  #for_each = google_cloudfunctions2_function.default_extract
 
-  project        = each.value.project
-  location       = each.value.location
-  cloud_function = each.value.name
+  project        = google_cloudfunctions2_function.default_extract.project
+  location       = google_cloudfunctions2_function.default_extract.location
+  cloud_function = google_cloudfunctions2_function.default_extract.name
 
   role   = var.role_type
   member = var.service_account
