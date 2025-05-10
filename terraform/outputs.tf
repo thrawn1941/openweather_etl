c20973d352cbf7d2892b89092b19988464c7e2fb
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-11 00:18:42 +0200

    feat(terraform): adding output into main output files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,6 @@ output "dataset_id" {
 output "pollution_history_pubsub_id" {
   value = google_pubsub_topic.extract_historical_pollution.id
 }
+output "module_pubsub_id" {
+  value = module.extract_functions.pubsub_topic_id
+}
