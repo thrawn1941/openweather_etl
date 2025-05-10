99437a26d6b6c2cc8a4e80486c5b61f61f90db44
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-11 01:01:06 +0200

    fix(terraform): testing pubsub_ids as local

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,9 +4,19 @@ output "dataset_id" {
 output "pollution_history_pubsub_id" {
   value = google_pubsub_topic.extract_historical_pollution.id
 }
-output "module_pubsub_ids" {
-  value = {
+#output "module_pubsub_ids" {
+#  value = {
+#    for k, m in module.extract_functions :
+#    k => m.pubsub_topic_id
+#  }
+#}
+
+locals {
+  pubsub_ids = {
     for k, m in module.extract_functions :
     k => m.pubsub_topic_id
   }
+}
+output "module_pubsub_ids" {
+  value = local.pubsub_ids
 }
