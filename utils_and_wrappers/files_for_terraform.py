bf50b64b32c500a7c0b0c7ac9ee4b8ab4c67e493
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-12 16:18:42 +0200

    feat(utils): adding create_openweather_workflow_file()

--- utils_and_wrappers/files_for_terraform.py
+++ utils_and_wrappers/files_for_terraform.py
@@ -28,6 +28,49 @@ def create_geo_workflow_file(region, project_id, function_name, topic_name):
     with open(file_path, "w") as file:
         file.write(text)
 
+def create_openweather_workflow_file(region, project_id, functions_list):
+    file_path = path + "openweather_workflow.yml"
+    file_path = file_path.replace('\\', '/')
+    functions_text=''
+    for function in functions_list:
+        functions_text+=f'\n                - {function}'
+
+    text=f"""    - init:
+        assign:
+            - results : {{}} # result from each iteration keyed by table name
+            - functions:{functions_text}
+    - runQueries:
+        parallel:
+            shared: [results]
+            for:
+                value: func
+                in: ${{functions}}
+                steps:
+                  - call_function:
+                      call: http.get
+                      args:
+                        url: ${{"https://{region}-{project_id}.cloudfunctions.net/" + func}}
+                        auth:
+                          type: OIDC
+                      result: function_response
+                  - publish_to_pubsub:
+                      call: googleapis.pubsub.v1.projects.topics.publish
+                      args:
+                        topic: ${{"projects/{project_id}/topics/" + func + "-topic"}}
+                        body:
+                          messages:
+                            - data: ${{base64.encode(text.encode(function_response.body))}}
+                      result: pubsub_response
+                  - returnResult:
+                      assign:
+                          - results[func]: ${{pubsub_response}}
+    - returnResults:
+        return: ${{results}}"""
+
+    with open(file_path, "w") as file:
+        file.write(text)
+
 def main():
     create_geo_workflow_file(region="europe-central2", project_id="totemic-client-447220-r1", function_name="extract_geo_data", topic_name="extract_geo_data-topic")
+    create_openweather_workflow_file(region="europe-central2", project_id="totemic-client-447220-r1", functions_list=["extract_pollution_data", "extract_weather_data"])
 main()
