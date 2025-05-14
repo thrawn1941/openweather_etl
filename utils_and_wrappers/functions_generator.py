c80757b756dc8d9f26b26321d18ebdb395a10909
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-14 12:36:15 +0200

    refactor(load): debug3

--- utils_and_wrappers/functions_generator.py
+++ utils_and_wrappers/functions_generator.py
@@ -1,11 +1,10 @@
 import base64
 import json
 from utils_and_wrappers.load import Load
-"""
 def create_load_function(target_table, load_strategy):
     def wrapper(func):
-        def inner(cloud_event):
-            imported_data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]))
+        def inner(*args):
+            imported_data = json.loads(base64.b64decode(args[0].data["data"]))
             if not imported_data:
                 print("No data provided!")
                 return
@@ -13,12 +12,5 @@ def create_load_function(target_table, load_strategy):
             imported_data=func(imported_data)
             load_app = Load(data=imported_data, target_table=target_table,load_strategy=load_strategy)
             load_app.load_raw_to_bigquery()
-        return inner
-    return wrapper
-"""
-def create_load_function(target_table, load_strategy):
-    def wrapper(func):
-        def inner(*args):
-            print(args)
         return inner
     return wrapper
