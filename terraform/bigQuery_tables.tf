8fcab11ed6720cc1368823e92d25a69f9f73c8ee
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-20 12:34:54 +0200

    refactor(terraform): removing partitioning from raw tables

--- terraform/bigQuery_tables.tf
+++ terraform/bigQuery_tables.tf
@@ -2,34 +2,22 @@ resource "google_bigquery_table" "weather_raw" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "weather_raw"
   schema = file("./support_files/bq_weather_schema.json")
-  time_partitioning {
-    type = "DAY"
-  }
 }
 
 resource "google_bigquery_table" "pollution_raw" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "pollution_raw"
   schema = jsonencode(var.bq_pollution_schema)
-  time_partitioning {
-    type = "DAY"
-  }
 }
 
 resource "google_bigquery_table" "pollution_raw_backup" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "pollution_raw_backup"
   schema = jsonencode(var.bq_pollution_schema)
-  time_partitioning {
-    type = "DAY"
-  }
 }
 
 resource "google_bigquery_table" "geo_raw" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "geo_raw"
   schema = jsonencode(var.bq_geo_schema)
-  time_partitioning {
-    type = "DAY"
-  }
 }
