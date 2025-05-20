1c12bf368df25048b92d4465403c9d24e116801d
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-20 13:33:08 +0200

    refactor(terraform): moving tables schemas from variable to json file

--- terraform/bigQuery_tables.tf
+++ terraform/bigQuery_tables.tf
@@ -7,17 +7,17 @@ resource "google_bigquery_table" "weather_raw" {
 resource "google_bigquery_table" "pollution_raw" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "pollution_raw"
-  schema = jsonencode(var.bq_pollution_schema)
+  schema = file("./support_files/bq_pollution_schema.json")
 }
 
 resource "google_bigquery_table" "pollution_raw_backup" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "pollution_raw_backup"
-  schema = jsonencode(var.bq_pollution_schema)
+  schema = file("./support_files/bq_pollution_schema.json")
 }
 
 resource "google_bigquery_table" "geo_raw" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "geo_raw"
-  schema = jsonencode(var.bq_geo_schema)
+  schema = file("./support_files/bq_geo_schema.json")
 }
