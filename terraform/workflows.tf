077f8ad5575006fc2625bc9fdbb7ddf7ef689d70
“thrawn1941” <aleksander.peszko1812@gmail.com>
2025-05-22 23:38:25 +0200

    refactor(terraform): adding trimprefix to workflows definitions

--- terraform/workflows.tf
+++ terraform/workflows.tf
@@ -3,7 +3,7 @@ resource "google_workflows_workflow" "openweather_workflow" {
   name            = "openweather-workflow"
   region          = "europe-central2"
   description     = "A workflow to run openweather extract functions"
-  service_account = var.service_account
+  service_account = trimprefix(var.service_account, "serviceAccount:")
 
   source_contents = file("./support_files/openweather_workflow.yml")
 
@@ -13,7 +13,7 @@ resource "google_workflows_workflow" "geo_workflow" {
   name            = "geo-workflow"
   region          = "europe-central2"
   description     = "Workflow exporting raw geo data to pubsub"
-  service_account = var.service_account
+  service_account = trimprefix(var.service_account, "serviceAccount:")
 
   source_contents = file("./support_files/geo_workflow.yml")
 
